df2cf3e9b4648887c97f94b176cedf57a7e4d860
CoolCoder0110 <simon.niklasson45@icloud.com>
2026-03-31 02:11:59 +0200

    Tredje uppladdningen

--- app.py
+++ app.py
@@ -21,5 +21,4 @@ def trigger_error():
     return {"status": "error", "message": "Simulerat fel under demo!"}, 500
 
 if __name__ == "__main__":
-    # nosec B104 (Binder till alla nätverkskort, designat i Docker/Kubernetes)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)  # nosec B104
